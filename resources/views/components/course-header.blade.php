39bfd7d88b5ab2f599fec8a2e4946bf65f720c9e
hyeucong <94946649+hyeucong@users.noreply.github.com>
2025-03-11 21:15:29 +0700

    cleanup

--- resources/views/components/course-header.blade.php
+++ resources/views/components/course-header.blade.php
@@ -1,22 +1,18 @@
 <flux:header class="block! bg-white lg:bg-zinc-50 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-700">
     <flux:navbar scrollable>
-        <flux:navbar.item wire:key="nav-overview" :href="route('course', $course->id)"
-            :current="request()->routeIs('course') || request()->is('*livewire/update*')" wire:navigate>
+        <flux:navbar.item :href="route('course', $course->id)" :current="request()->routeIs('course')" wire:navigate>
             Overview</flux:navbar.item>
-        <flux:navbar.item wire:key="nav-stream" :href="route('stream', $course->id)"
-            :current="request()->routeIs('stream')" wire:navigate>
+        <flux:navbar.item :href="route('stream', $course->id)" :current="request()->routeIs('stream')" wire:navigate>
             Stream</flux:navbar.item>
-        <flux:navbar.item wire:key="nav-attendance" :href="route('attendance', $course->id)"
-            :current="request()->routeIs('attendance')" wire:navigate>Attendance</flux:navbar.item>
-        <flux:navbar.item wire:key="nav-people" badge="32" :href="route('people', $course->id)"
-            :current="request()->routeIs('people')" wire:navigate>People</flux:navbar.item>
-        <flux:navbar.item wire:key="nav-grades" :href="route('grades', $course->id)"
-            :current="request()->routeIs('grades')" wire:navigate>
+        <flux:navbar.item :href="route('attendance', $course->id)" :current="request()->routeIs('attendance')"
+            wire:navigate>Attendance</flux:navbar.item>
+        <flux:navbar.item badge="32" :href="route('people', $course->id)" :current="request()->routeIs('people')"
+            wire:navigate>People</flux:navbar.item>
+        <flux:navbar.item :href="route('grades', $course->id)" :current="request()->routeIs('grades')" wire:navigate>
             Grades</flux:navbar.item>
-        <flux:navbar.item wire:key="nav-details" :href="route('details', $course->id)"
-            :current="request()->routeIs('details')" wire:navigate>
+        <flux:navbar.item :href="route('details', $course->id)" :current="request()->routeIs('details')" wire:navigate>
             Details</flux:navbar.item>
-        <flux:navbar.item wire:key="nav-settings" :href="route('settings', $course->id)"
-            :current="request()->routeIs('settings')" wire:navigate>Settings</flux:navbar.item>
+        <flux:navbar.item :href="route('settings', $course->id)" :current="request()->routeIs('settings')"
+            wire:navigate>Settings</flux:navbar.item>
     </flux:navbar>
 </flux:header>
