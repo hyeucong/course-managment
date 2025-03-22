7af010943d5461878ac70389c24927466c4e6a53
hyeucong <94946649+hyeucong@users.noreply.github.com>
2025-03-22 15:10:30 +0700

    update landing page

--- resources/views/components/layouts/client/problem.blade.php
+++ resources/views/components/layouts/client/problem.blade.php
@@ -1,71 +1,52 @@
 <section class="bg-white py-32 px-4">
     <div class="max-w-7xl mx-auto text-center px-6">
         <h2 class="text-4xl md:text-5xl font-bold tracking-tight text-gray-900 mb-4">
-            You're moving fast but <br> your tools can't keep up
+            You're clicking fast but <br> your tools can't keep up
         </h2>
 
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-12">
-            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div class="flex-1 relative overflow-hiddenflex justify-center items-center">
-                    <div class="bg-white rounded-xl shadow-lg p-6 border border-gray-200">
-                        <div class="flex items-center mb-4">
-                            <span class="mr-3 text-red-500">
-                                <flux:icon.bolt />
-
-                            </span>
-                            <h3 class="text-lg font-semibold text-gray-800">You're stuck in spreadsheets</h3>
-                        </div>
-                        <p class="text-gray-600">
-                            Spreadsheets and low-code tools are fantastic to start with but quickly pile up and become
-                            impossible to maintain
-                        </p>
+            <div class="p-1 flex rounded-[12px] border border-zinc-200 shadow-xs ">
+                <div class="bg-white rounded-xl shadow-lg p-6 border border-gray-200">
+                    <div class="flex items-center mb-2">
+                        <span class="mr-3 text-red-500">
+                            <flux:icon.bolt />
+                        </span>
+                        <h3 class="text-lg font-semibold text-gray-800">You're stuck in manual records</h3>
                     </div>
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
+                    <p class="text-gray-600">
+                        Basic digital tools are fine to start with but quickly become disorganized
+                        and difficult to track as your student numbers grow
+                    </p>
                 </div>
             </div>
 
-
-            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div class="flex-1 relative overflow-hiddenflex justify-center items-center">
-                    <div class="bg-white rounded-xl shadow-lg p-6 border border-gray-200">
-                        <div class="flex items-center mb-4">
-                            <span class="mr-3 text-red-500">
-                                <flux:icon.bolt />
-
-                            </span>
-                            <h3 class="text-lg font-semibold text-gray-800">You're stuck in spreadsheets</h3>
-                        </div>
-                        <p class="text-gray-600">
-                            Spreadsheets and low-code tools are fantastic to start with but quickly pile up and become
-                            impossible to maintain
-                        </p>
+            <div class="p-1 flex rounded-[12px] border border-zinc-200 shadow-xs ">
+                <div class="bg-white rounded-xl shadow-lg p-6 border border-gray-200">
+                    <div class="flex items-center mb-2">
+                        <span class="mr-3 text-red-500">
+                            <flux:icon.bolt />
+                        </span>
+                        <h3 class="text-lg font-semibold text-gray-800">Your teaching isn't "one-fits-all"</h3>
                     </div>
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
+                    <p class="text-gray-600">
+                        You're spending valuable time waiting for your tools to load, or you're stuck using
+                        platforms that don't understand your needs
+                    </p>
                 </div>
             </div>
 
-            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div class="flex-1 relative overflow-hiddenflex justify-center items-center">
-                    <div class="bg-white rounded-xl shadow-lg p-6 border border-gray-200">
-                        <div class="flex items-center mb-4">
-                            <span class="mr-3 text-red-500">
-                                <flux:icon.bolt />
-
-                            </span>
-                            <h3 class="text-lg font-semibold text-gray-800">You're stuck in spreadsheets</h3>
-                        </div>
-                        <p class="text-gray-600">
-                            Spreadsheets and low-code tools are fantastic to start with but quickly pile up and become
-                            impossible to maintain
-                        </p>
+            <div class="p-1 flex rounded-[12px] border border-zinc-200 shadow-xs ">
+                <div class="bg-white rounded-xl shadow-lg p-6 border border-gray-200">
+                    <div class="flex items-center mb-2">
+                        <span class="mr-3 text-red-500">
+                            <flux:icon.bolt />
+                        </span>
+                        <h3 class="text-lg font-semibold text-gray-800">You're held back by low-code tools</h3>
                     </div>
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
+                    <p class="text-gray-600">
+                        Traditional management systems are complicated, making it difficult to
+                        adapt to your evolving curriculum and teaching
+                    </p>
                 </div>
             </div>
         </div>
--- resources/views/components/layouts/client/product.blade.php
+++ resources/views/components/layouts/client/product.blade.php
@@ -1,74 +1,60 @@
 <section class="bg-gray-50 py-20 px-4">
     <div class="max-w-7xl mx-auto px-6">
         <div class="text-center mb-12">
-            <h2 class="text-4xl md:text-5xl font-bold tracking-tight text-gray-900">We've got your needs covered</h2>
+            <h2 class="text-4xl md:text-5xl font-bold tracking-tight text-gray-900">We've got your needs
+                covered</h2>
             <p class="mt-4 text-gray-600">
-                Konstantly offers a wide range of capabilities to help businesses enhance their employees' <br>
-                knowledge and deliver improved support to both employees and customers.
+                LectureSpace provides educators with powerful tools to create engaging learning environments and
+                streamline classroom management.
             </p>
         </div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div
-                    class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200 flex justify-center items-center">
-                    <img src="https://images.unsplash.com/photo-1740166260052-b41e5381bcbb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="">
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
+                <div class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200">
+                    <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3"
+                        alt="Dashboard showing all course features" class="w-full h-full object-cover">
                 </div>
                 <div class="flex flex-col gap-1 px-4 py-3">
-                    <div class="text-sm font-medium text-zinc-800 ">Rich text editor</div>
-                    <div class="text-sm text-zinc-500 ">A powerful rich text editor component</div>
-                </div>
-            </div>
-
-
-            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div
-                    class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200 flex justify-center items-center">
-                    <img src="https://images.unsplash.com/photo-1740166260052-b41e5381bcbb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="">
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
-                </div>
-                <div class="flex flex-col gap-1 px-4 py-3">
-                    <div class="text-sm font-medium text-zinc-800 ">Rich text editor</div>
-                    <div class="text-sm text-zinc-500 ">A powerful rich text editor component</div>
-                </div>
-            </div>
-
-
-            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div
-                    class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200 flex justify-center items-center">
-                    <img src="https://images.unsplash.com/photo-1740166260052-b41e5381bcbb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="">
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
-                </div>
-                <div class="flex flex-col gap-1 px-4 py-3">
-                    <div class="text-sm font-medium text-zinc-800 ">Rich text editor</div>
-                    <div class="text-sm text-zinc-500 ">A powerful rich text editor component</div>
+                    <div class="text-sm font-medium text-zinc-800">Intuitive Modern Interface</div>
+                    <div class="text-sm text-zinc-500">Clean, responsive design that just works.</div>
                 </div>
             </div>
 
             <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
-                <div
-                    class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200 flex justify-center items-center">
-                    <img src="https://images.unsplash.com/photo-1740166260052-b41e5381bcbb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt="">
-                </div>
-                <div
-                    class="pointer-events-none absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-zinc-100to-transparent">
+                <div class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200">
+                    <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3"
+                        alt="Dashboard showing all course features" class="w-full h-full object-cover">
                 </div>
                 <div class="flex flex-col gap-1 px-4 py-3">
-                    <div class="text-sm font-medium text-zinc-800 ">Rich text editor</div>
-                    <div class="text-sm text-zinc-500 ">A powerful rich text editor component</div>
+                    <div class="text-sm font-medium text-zinc-800">Effortless Class Creation</div>
+                    <div class="text-sm text-zinc-500">Set up new courses in minutes.</div>
+                </div>
+            </div>
+
+            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
+                <div class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200">
+                    <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3"
+                        alt="Dashboard showing all course features" class="w-full h-full object-cover">
+                </div>
+                <div class="flex flex-col gap-1 px-4 py-3">
+                    <div class="text-sm font-medium text-zinc-800">All features in one place</div>
+                    <div class="text-sm text-zinc-500">Assignments and enrollments and more from a
+                        centralized dashboard.
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-1 flex flex-col gap-1 rounded-[12px] border border-zinc-200 shadow-xs">
+                <div class="flex-1 relative overflow-hidden rounded-[8px] bg-zinc-100 border border-zinc-200">
+                    <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3"
+                        alt="Dashboard showing all course features" class="w-full h-full object-cover">
+                </div>
+                <div class="flex flex-col gap-1 px-4 py-3">
+                    <div class="text-sm font-medium text-zinc-800">Say goodbye to manual course positioning</div>
+                    <div class="text-sm text-zinc-500">Search or viewing the course the way you like</div>
                 </div>
             </div>
         </div>
+    </div>
 </section>
